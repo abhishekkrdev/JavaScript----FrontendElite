4b129931f8250f54eef570d2e256699536680857
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2024-12-28 04:13:36 +0530

    Added  Scopes Code

---
+++ 3)SecondPillarJS-Scopes/b)globalscope.js
@@ -0,0 +1,7 @@
+// nameAuthor is a global Scope
+var nameAuthor = "Sanket";
+function fun() {
+    console.log(nameAuthor);
+}
+fun();
+console.log(nameAuthor);
---
+++ 3)SecondPillarJS-Scopes/h)whichscoping.js
@@ -0,0 +1,16 @@
+// var teacher = "Sanket";
+// function ask(question) {
+// 	console.log(teacher, question);
+// }
+
+// function fun() {
+// 	var teacher = "Pulkit";  // In dynamic scoping ask would have taken this teacher
+// 	ask("why?");
+// }
+// fun();
+var fun;
+function fun() {
+    return "fun2";
+}
+
+console.log(fun);
